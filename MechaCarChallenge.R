cd870547709efb5099a36fee845e5cadc5473974
josafathpelayo <77094829+josafathpelayo@users.noreply.github.com>
2021-05-16 13:54:05 -0700

    first commit

---
+++ MechaCarChallenge.R
@@ -0,0 +1,9 @@
+library(dplyr)
+MechCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data= MechCar_table) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data= MechCar_table))
+
+
+
+
+
